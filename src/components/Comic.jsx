d23d4daa8fbf5f2b86b87607643fc9178f2b309b
parthpandyappp <parthpandyappp@gmail.com>
2021-09-20 15:03:21 +0530

    Updated the code with succesful fetch script of the API call and avoiding the CORS issue. Now supporting Customised and Personalized fetch.

--- src/components/Comic.jsx
+++ src/components/Comic.jsx
@@ -1,29 +1,87 @@
 import React, { useEffect, useState } from "react";
 
 function Comic() {
-  var api = "https://xkcd.com/info.0.json";
+  var [imageCom, setImg] = useState("");
+  const [latestRef, setLatestRef] = useState("");
+  var [comicRef, setRef] = useState("");
+  var [loading, setLoading] = useState(false);
+  var [render, setRender] = useState(false);
 
-  const fetchAPI = () => {
-    // fetch(api, {
-    //   headers: {
-    //     "Access-Control-Allow-Origin": "*",
-    //     "Content-Type": "application/json",
-    //   },
-    //   mode: "no-cors",
-    // }).then(function (response) {
-    //   console.log(response.json());
-    // });
-    fetch(api, { mode: "no-cors" })
-      .then(function (response) {
-        console.log(response.text());
-      })
-      .then(function (text) {
-        console.log("Request successful", text);
-      })
-      .catch(function (error) {
-        console.log("Request failed", error);
-      });
+  var corsHeader = "https://the-ultimate-api-challenge.herokuapp.com/";
+  var mains = "https://xkcd.com/";
+  var latest = "info.0.json";
+  var api = "";
+
+  const latestFetch = () => {
+    api = corsHeader + mains + latest;
+    fetchAPI(api);
   };
+
+  const prevFetch = () => {
+    var ref = (comicRef - 1).toString();
+    setRef(comicRef - 1);
+    api = corsHeader + mains + ref + "/" + latest;
+    fetchAPI(api);
+  };
+
+  const nextFetch = () => {
+    var ref;
+    if (comicRef + 1 > latestRef) {
+      ref = comicRef.toString();
+    } else {
+      ref = (comicRef + 1).toString();
+      setRef(comicRef + 1);
+    }
+    api = corsHeader + mains + ref + "/" + latest;
+    fetchAPI(api);
+  };
+
+  const randomFetch = () => {
+    var max = Math.floor(latestRef + 1);
+    console.log(max);
+    randomRef = Math.floor(Math.random() * max);
+    api = corsHeader + mains + randomRef + "/" + latest;
+    console.log(api);
+    fetchAPI(api);
+  };
+
+  async function fetchAPI(api) {
+    const res = await fetch(api);
+    console.log("status:", res.ok, res);
+
+    if (!res.ok) {
+      const err = await res.text();
+      console.log("err:", err);
+    } else {
+      setLoading(true);
+      let data = await res.json();
+      setImg(data.img);
+      setRef(data.num);
+
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+      setRender(true);
+      console.log(imageCom);
+    }
+  }
+
+  useEffect(async function fetchAPI() {
+    const res = await fetch(
+      "https://the-ultimate-api-challenge.herokuapp.com/https://xkcd.com/info.0.json"
+    );
+    console.log("status:", res.ok, res);
+
+    if (!res.ok) {
+      const err = await res.text();
+      console.log("err:", err);
+    } else {
+      let data = await res.json();
+
+      setRef(data.num);
+      setLatestRef(data.num);
+    }
+  }, []);
 
   return (
     <div className="flex flex-col justify-center">
@@ -31,33 +89,54 @@ function Comic() {
         {/* <img src="" > */}
         <button
           className="bg-gray-600 text-white font-bold py-2 px-4 rounded hover:shadow-lg"
-          onClick={fetchAPI}
+          onClick={prevFetch}
         >
           Prev
         </button>
         <button
           className="bg-gray-600 text-white font-bold py-2 px-4 rounded hover:shadow-lg"
-          onClick={fetchAPI}
+          onClick={latestFetch}
         >
           Latest
         </button>
         <button
           className="bg-gray-600 text-white font-bold py-2 px-4 rounded hover:shadow-lg"
-          onClick={fetchAPI}
+          onClick={nextFetch}
         >
           Next
         </button>
       </div>
-      <div className="flex justify-center items-center mt-10 md:mt-6 p-2">
-        <img
-          className="shadow-lg"
-          src="https://lh3.googleusercontent.com/proxy/4n5qb-ojgjSGxB1ahoAwHk4F1xeELw2iosnJoyP-saKiYD0Q2uQgEF2utlsvJzGyHktY09Ah7oLtY-Yr2yjoTp65OvumsDVhkCr8cq2jwpjSjdH2OaI"
-        />
-      </div>
+
+      {loading ? (
+        <div className="flex justify-center items-center mt-10 md:mt-6 p-2">
+          <i className="fa fa-cog fa-spin text-4xl" />
+        </div>
+      ) : (
+        [
+          render ? (
+            <div className="flex justify-center items-center mt-10 md:mt-6 p-2">
+              <img
+                className="shadow-lg"
+                src={imageCom}
+                height="200"
+                width="200"
+              />
+            </div>
+          ) : (
+            <div className="flex justify-center items-center mt-10 md:mt-6 p-2">
+              <img
+                className="shadow-lg"
+                src="https://imgs.xkcd.com/comics/rover_replies.png"
+              />
+            </div>
+          ),
+        ]
+      )}
+
       <div className="flex text-center justify-center items-center space-x-2 mt-4">
         <button
           className="bg-gray-600 text-white text-lg font-bold px-6 py-2 rounded hover:shadow-lg"
-          onClick={fetchAPI}
+          onClick={randomFetch}
         >
           Random
         </button>
